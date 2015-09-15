7249b0543e13611f52c449d6c8ad93ee67880c00
notxyz <chenchen0829@gmail.com>
2015-09-14 22:02:34 -0400

    Test Pass

--- digragh.py
+++ digragh.py
@@ -1,9 +1,12 @@
+"""
+Algorithmic Thinking Project 1
+"""
 # Constants
-EX_GRAPH0 = {0: set([1, 2])}
+EX_GRAPH0 = {0: set([1, 2]), 1: set(), 2: set()}
 EX_GRAPH1 = {0: set([1, 4, 5]), 1: set([2, 6]),
              2: set([3]), 3: set([0]), 4: set([1]),
              5: set([2]), 6: set()}
-EX_GRAPH2 = {0: set(1, 4, 5), 1: set([2, 6]),
+EX_GRAPH2 = {0: set([1, 4, 5]), 1: set([2, 6]),
              2: set([3, 7]), 3: set([7]), 4: set([1]),
              5: set([2]), 6: set(), 7: set([3]),
              8: set([1, 2]), 9: set([0, 3, 4, 5, 6, 7])}
@@ -31,7 +34,7 @@ def compute_in_degrees(digraph):
     """
     
     in_degree_dic = dict()
-    for key in range(digraph):
+    for key in digraph.keys():
         degree = 0
         for node, edges in digraph.items():
             if node == key:
@@ -47,15 +50,17 @@ def in_degree_distribution(digraph):
     Takes a directed graph digraph and computes the unnormalized
     distribution of the in-degrees of the graph
     """
+    digraph_degree = compute_in_degrees(digraph)
+    in_degree_dist = dict()
     
-    in_degree_distribution = dict()
-    num_nodes = len(digraph)
-    
-    for key in range(0, len(digraph) - 1):
+    for key in range(0, len(digraph_degree)):
         count = 0
-        for degree in range(digraph.values()):
+        for degree in digraph_degree.values():
             if degree == key:
                 count += 1
-        in_degree_distribution.update({key: count/num_nodes})
+                print count
+        if count > 0:
+            in_degree_dist.update({key: count})
                    
-    return in_degree_distribution
+    return in_degree_dist
+  
